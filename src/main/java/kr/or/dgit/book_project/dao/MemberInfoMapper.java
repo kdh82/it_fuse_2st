9b6caf4ac2ee9c2326754bbd74b81d9c20c8b1b1
lkm <rksym343@naver.com>
2017-04-04 12:37:47 +0900

    layout 수정

--- src/main/java/kr/or/dgit/book_project/dao/MemberInfoMapper.java
+++ src/main/java/kr/or/dgit/book_project/dao/MemberInfoMapper.java
@@ -1,7 +1,5 @@
 package kr.or.dgit.book_project.dao;
 
-import kr.or.dgit.book_project.dto.MemberInfo;
-
 public interface MemberInfoMapper {
-	
+
 }
--- src/main/java/kr/or/dgit/book_project/ui/common/AbsViewPanel.java
+++ src/main/java/kr/or/dgit/book_project/ui/common/AbsViewPanel.java
@@ -5,29 +5,19 @@ import java.awt.Rectangle;
 import javax.swing.JPanel;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
+import javax.swing.border.EmptyBorder;
 
 public class AbsViewPanel extends JPanel {
 
 	protected JPanel pMain;
 
 	public AbsViewPanel() {
+		setBorder(new EmptyBorder(20, 20, 20, 20));
 		setBounds(new Rectangle(0, 0, 775, 600));
-		setLayout(new BorderLayout(10, 10));
-		
-		JPanel panel = new JPanel();
-		add(panel, BorderLayout.NORTH);
-		
-		JPanel panel_1 = new JPanel();
-		add(panel_1, BorderLayout.SOUTH);
-		
-		JPanel panel_2 = new JPanel();
-		add(panel_2, BorderLayout.WEST);
-		
-		JPanel panel_3 = new JPanel();
-		add(panel_3, BorderLayout.EAST);
+		setLayout(new GridLayout(0, 1, 0, 0));
 		
 		pMain = new JPanel();
-		add(pMain, BorderLayout.CENTER);
+		add(pMain);
 		pMain.setLayout(new GridLayout(0, 1, 0, 0));
 	}
 	
