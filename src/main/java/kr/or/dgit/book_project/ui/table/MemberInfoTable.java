69b47126c1c71946fec37b4591b97a81e576fce5
inyoung <inyoung.estrella6y@naver.com>
2017-04-06 09:26:33 +0900

    -

---
+++ src/main/java/kr/or/dgit/book_project/ui/table/MemberInfoTable.java
@@ -0,0 +1,55 @@
+package kr.or.dgit.book_project.ui.table;
+
+import kr.or.dgit.book_project.dto.MemberInfo;
+
+public class MemberInfoTable extends AbsTable<MemberInfo> {
+
+	@Override
+	protected void createPopupMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void updateData(MemberInfo t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void deleteItem(MemberInfo t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void cellWith() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void CellAlign() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected Object[][] getRowData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Object[] getColumn() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MemberInfo getSelectedObject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
