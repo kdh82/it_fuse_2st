362ac6a0147c2e2bb9b0dbb1d4d18153e115b9e7
lkm <rksym343@naver.com>
2017-04-08 14:20:34 +0900

    탭 선택시 load 되도록

--- src/main/java/kr/or/dgit/book_project/ui/SubMenuPage0.java
+++ src/main/java/kr/or/dgit/book_project/ui/SubMenuPage0.java
@@ -2,6 +2,8 @@ package kr.or.dgit.book_project.ui;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -18,65 +20,75 @@ import javax.swing.event.ChangeEvent;
 
 public class SubMenuPage0 extends JTabbedPane implements ChangeListener {
 
-private JPanel pMember;
-private JPanel pBook;
-private JPanel pManager;
-private JPanel pPublisher;
-private JPanel pCoden;
-private JPanel pBookManager;
-private JPanel pDiscardBook;
+	private JPanel pMember;
+	private JPanel pBook;
+	private JPanel pManager;
+	private JPanel pPublisher;
+	private JPanel pCoden;
+	private JPanel pBookManager;
+	private JPanel pDiscardBook;
+	private BookManageView bookManageView;
+	private PublisherView publisherView;
+	private DiscardBookManage discardBookManage;
+	private CodenManageView codenManagerView;
 
 	public SubMenuPage0() {
-		addChangeListener(this);		
-		pBook = new JPanel();		
+		addChangeListener(this);
+		pBook = new JPanel();
+
 		addTab("도서등록", null, pBook, null);
 		pBook.setLayout(new GridLayout(0, 1, 0, 0));
 		BookInsertView bookInsertview = new BookInsertView();
 		pBook.add(bookInsertview);
-		
+
 		pBookManager = new JPanel();
 		addTab("도서관리", null, pBookManager, null);
-		pBookManager.setLayout(new GridLayout(0, 1, 0, 0));
-		BookManageView bookManageView = new BookManageView();
-		pBookManager.add(bookManageView);
-		
+
 		pDiscardBook = new JPanel();
 		addTab("폐기도서", null, pDiscardBook, null);
-		pDiscardBook.setLayout(new BorderLayout(0, 0));
-		DiscardBookManage discardBookManage = new DiscardBookManage();
-		pDiscardBook.add(discardBookManage);
-		
+
 		pPublisher = new JPanel();
 		addTab("출판사관리", null, pPublisher, null);
-		pPublisher.setLayout(new GridLayout(0, 1, 0, 0));
-		PublisherView publisherView = new PublisherView();
-		pPublisher.add(publisherView);
-		
+
 		pCoden = new JPanel();
 		addTab("분류관리", null, pCoden, null);
-		pCoden.setLayout(new GridLayout(0, 1, 0, 0));
-		CodenManageView codenManagerView = new CodenManageView();
-		pCoden.add(codenManagerView);
-	}	
+
+	}
+
 	public void stateChanged(ChangeEvent e) {
 		if (e.getSource() == this) {
-			System.out.println(e.getSource());
-			JOptionPane.showMessageDialog(null, "도서등록");
+			// 선택된 탭의 idx를 넘겨줌
+			stateChangedThis(this.getSelectedIndex());
 		}
-		if (e.getSource() == pBookManager) {
-			JOptionPane.showMessageDialog(null, "도서관리");
-		}
-		if (e.getSource() == pDiscardBook) {
-			JOptionPane.showMessageDialog(null, "폐기도서");
-		}
-		if (e.getSource() == pPublisher) {
-			JOptionPane.showMessageDialog(null, "출판사");
-		}
-		if (e.getSource() == pCoden) {
-			JOptionPane.showMessageDialog(null, "분류");
-		}
+
 	}
-	protected void stateChangedThis(ChangeEvent e) {
-	
+
+	private void stateChangedThis(int idx) {
+		if (this.getTitleAt(idx).equals("도서관리") && bookManageView == null) {
+			// 선택된 탭의 제목에 따라서 조건 지정
+			pBookManager.setLayout(new GridLayout(0, 1, 0, 0));
+			bookManageView = new BookManageView();
+			pBookManager.add(bookManageView);
+
+		} else if (this.getTitleAt(idx).equals("폐기도서") && discardBookManage == null) {
+			pDiscardBook.setLayout(new BorderLayout(0, 0));
+			discardBookManage = new DiscardBookManage();
+			pDiscardBook.add(discardBookManage);
+		} else if (this.getTitleAt(idx).equals("출판사관리") && publisherView == null) {
+			pPublisher.setLayout(new GridLayout(0, 1, 0, 0));
+			publisherView = new PublisherView();
+			pPublisher.add(publisherView);
+		} else if (this.getTitleAt(idx).equals("분류관리") && codenManagerView == null) {
+			pCoden.setLayout(new GridLayout(0, 1, 0, 0));
+			codenManagerView = new CodenManageView();
+			pCoden.add(codenManagerView);
+		}
+
 	}
+
+	/*
+	 * JTabbedPane sourceTabbedPane = (JTabbedPane) changeEvent.getSource(); int
+	 * index = sourceTabbedPane.getSelectedIndex(); System.out.println(
+	 * "Tab changed to: " + sourceTabbedPane.getTitleAt(index));
+	 */
 }
