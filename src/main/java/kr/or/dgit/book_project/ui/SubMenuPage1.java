ce0f33b6fcf42ee5b90d24f55070cb365335988d
S <rksym@naver.com>
2017-04-09 00:40:19 +0900

    회원등급별 다른 페이지 뜨게 만들기 완성..

--- src/main/java/kr/or/dgit/book_project/ui/SubMenuPage1.java
+++ src/main/java/kr/or/dgit/book_project/ui/SubMenuPage1.java
@@ -24,7 +24,7 @@ public class SubMenuPage1 extends JTabbedPane implements ChangeListener {
 	private MemberSearchComboView memberSearchComboView;
 	private MemberInsertView memberInsertViewEmp;
 
-	public SubMenuPage1() {
+	public SubMenuPage1(char mGroup) {
 		addChangeListener(this);
 		pMember = new JPanel();
 		addTab("회원등록", null, pMember, null);
@@ -32,12 +32,28 @@ public class SubMenuPage1 extends JTabbedPane implements ChangeListener {
 		MemberInsertView memberInsertView = new MemberInsertView();
 		pMember.add(memberInsertView);
 
-		pMemberManager = new JPanel();
-		addTab("회원관리", null, pMemberManager, null);
+		setSubMenuPage1(mGroup);
 
-		pManager = new JPanel();
-		addTab("직원관리", null, pManager, null);
+	}
 
+	public void setSubMenuPage1(char mGroup) {
+		switch (mGroup) {
+		case 'A':
+			// 관리자..직원메뉴까지 볼수있음
+			pMemberManager = new JPanel();
+			addTab("회원관리", null, pMemberManager, null);
+
+			pManager = new JPanel();
+			addTab("직원관리", null, pManager, null);
+			break;
+		case 'B':
+			// 사서.... 직원메뉴 제외 전부 볼 수 있음..
+			pMemberManager = new JPanel();
+			addTab("회원관리", null, pMemberManager, null);
+			break;
+		default:
+			break;
+		}
 	}
 
 	public void stateChanged(ChangeEvent e) {
